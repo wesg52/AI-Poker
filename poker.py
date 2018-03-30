71b3912f6fb3812cf6b8085181d3ee5f4a00e033
Jachua <35241857+Jachua@users.noreply.github.com>
2018-03-30 17:41:55 -0400

    Add files via upload
    A simplified skeleton of some of the functions we will need to implement.

---
+++ poker.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def vectorize(card):
+    """Returns the vector representing the card"""
+    pass
+
+def classifier(w1, w2, w3, x):
+    """w1, w2, w3... : weights that determine the action
+    x: vector representing the hand.
+    Returns the action to be made by the agent in the current round."""
+    ##if np.dot(w, x) > 0 then ...
+    pass
+
+def NBClassfier(X, Y):
+    """X: the n * m input matrix 
+    Y: n * 1 labels
+    Returns the naive bayes classfier w."""
+    pass
+
+def self_play(w1, w2, w3):
+    """Plays the game against it self for n times
+    Returns X the n*m input matrix and Y the n * 1 labels."""
+    pass 
