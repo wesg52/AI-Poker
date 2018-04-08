8654d7530603843cb1f0cac1e0fd217ec6881fe0
Jachua <35241857+Jachua@users.noreply.github.com>
2018-04-07 21:31:36 -0400

    Update data.py

--- data.py
+++ data.py
@@ -16,20 +16,28 @@ def NBClassfier(X, Y):
     Returns the naive bayes classfier w."""
     pass
 
-def self_play():
-    m = 52
-    w = np.zeros(m)
+def data():
     player_names = ["p1", "p2", "p3", "p4", "p5", "p6", "p7", "p8", "p9"]
+    player_ids = ["id1", "id2", "id3", "id4", "id5", "id6", "id7", "id8", "id9"]
     for i in 10:
         emuluator = Emulator()
         num = np.random.randint(10)
         emulator.set_game_rule(nb_player, final_round, sb_amount, ante)
         emulator.set_game_rule(player_num=num, max_round=10, small_blind_amount=5, ante_amount=1)
         config = setup_config(max_round=10, initial_stack=100, small_blind_amount=5)
-        for p in num: 
+        players_info = {}
+        for p in num:
+            players_info.update(player_ids[p], {"name": player_names[p], "stack": 100})
             config.register_player(name=player_names[p], algorithm=NaiveBot())
-        #staet = ...
-        #emulator.run_until_game_finish(state)
-        X, Y = self_play(w, w, w, 10**i)
-        w = NBClassfier(X, Y)
+        initial_state = emulator.generate_initial_game_state(players_info)
+        game_state, events = emulator.start_new_round(initial_state)
+        game_state, _ = emulator.run_until_game_finish(game_state)
+        X = np.zeros(num, 112)
+        Y = np.zeros(num, 3)
+        players = game_state["table"].seats.players
+        for p in num:
+            player = players.get(player_names[p])
+            cards = player.hole_card
+            X[p] = NaiveBot("").get_input_vector(game_state, cards)
+    return X, Y
 
