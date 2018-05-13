a8478bd9abb8b8150b78746afc05f908ba4ae7f4
Jachua <35241857+Jachua@users.noreply.github.com>
2018-05-13 07:12:38 -0400

    Update eval.py

--- eval.py
+++ eval.py
@@ -2,8 +2,10 @@ import pickle
 from console_player import ConsolePlayer
 from pypokerengine.api.emulator import Emulator
 from bot import PGBot
-from pypokerengine.utils.game_state_utils import attach_hole_card_from_deck, attach_hole_card
+from pypokerengine.utils.game_state_utils import attach_hole_card_from_deck, attach_hole_card, replace_community_card
 from pypokerengine.utils.card_utils import gen_cards
+
+SAVE_PATH = 'saved_models/second_test/policy_net_after'
 
 STACK = 1000
 N_PLAYER = 2
@@ -15,7 +17,7 @@ if __name__ == '__main__':
     emul = Emulator()
     emul.set_game_rule(N_PLAYER, MAX_ROUND, SMALL_BLIND_AMOUNT, ANTE_AMOUNT)
     console = ConsolePlayer()
-    bot = PGBot("bot", pickle.load(open("105980", "rb")))
+    bot = PGBot("bot", pickle.load(open(SAVE_PATH + "105980", "rb")))
     emul.register_player("target", bot)
     emul.register_player("baseline", console)
     players_info = {
@@ -38,6 +40,32 @@ if __name__ == '__main__':
             else:
                 game_state = attach_hole_card_from_deck(game_state, player.uuid)
         game_state, events = emul.run_until_round_finish(game_state)
+        while game_state["street"] != Const.Street.FINISHED:
+            print(game_state["street"])
+            cur_street = game_state["street"]
+            print(events[-1]['valid_actions'])
+            action = input("Enter a valid action > ")
+            amount = int(input("Enter a valid amount > "))
+            game_state, events = emul.apply_action(game_state, action, amount)
+            if game_state["street"] != cur_street:
+                if events[0]["round_state"]["street"] == "flop":
+                    flop1 = input("Enter first flop > ")
+                    flop2 = input("Enter second flop > ")
+                    flop3  = input("Enter third flop > ")
+                    game_state = replace_community_card(game_state, gen_cards([flop1, flop2, flop3]))
+                elif events[0]["round_state"]["street"] == "turn":
+                    flop1 = input("Enter first flop > ")
+                    flop2 = input("Enter second flop > ")
+                    flop3  = input("Enter third flop > ")
+                    turn = input("Enter turn card > ")
+                    game_state = replace_community_card(game_state, gen_cards([flop1, flop2, flop3, turn]))
+                elif events[0]["round_state"]["street"] == "river":
+                    flop1 = input("Enter first flop > ")
+                    flop2 = input("Enter second flop > ")
+                    flop3  = input("Enter third flop > ")
+                    turn = input("Enter turn card > ")
+                    river = input("Enter river card > ")
+                    game_state = replace_community_card(game_state, gen_cards([flop1, flop2, flop3, turn, river]))
         if events[0]["round_state"]["round_count"] >= MAX_ROUND:
             break
 
